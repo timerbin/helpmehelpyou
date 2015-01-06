6cbe20eb94583a93b900cccc5d8aec39a13b9db6
timerbin <wangwenbin8023@sina.com>
2015-01-06 21:40:39 +0800

    20150106 test

---
+++ src/main/java/com/timerbin/config/BseConfig.java
@@ -0,0 +1,62 @@
+package com.timerbin.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.timerbin.controller.*;
+
+
+public class BseConfig  extends JFinalConfig {
+
+	/**
+	 * 配置常量
+	 */
+	@Override
+	public void configConstant(Constants me) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * 配置路由
+	 */
+	@Override
+	public void configRoute(Routes me) {
+		me.add("/", IndexController.class,"/template");
+	}
+	/**
+	 * 配置插件
+	 */
+	@Override
+	public void configPlugin(Plugins me) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * 配置全局拦截器
+	 */
+	@Override
+	public void configInterceptor(Interceptors me) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * 配置处理器
+	 */
+	@Override
+	public void configHandler(Handlers me) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	/**
+//	 * 建议使用 JFinal 手册推荐的方式启动项目
+//	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
+//	 */
+//	public static void main(String[] args) {
+//		JFinal.start("WebRoot", 8081, "/", 5);
+//	}
+
+}
---
+++ src/main/java/com/timerbin/controller/IndexController.java
@@ -0,0 +1,9 @@
+package com.timerbin.controller;
+
+import com.jfinal.core.Controller;
+
+public class IndexController extends Controller {
+	public void index() {
+		render("index.html");
+	}
+}
